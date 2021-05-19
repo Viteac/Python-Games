8bf7ed9c88f066bf7bb8571d0169270e9751615e
Vimuster <32931324+Viteac@users.noreply.github.com>
2021-05-19 19:44:20 +0100

    Python Guess The Numer Game
    Beginner Python Practice Project: Guess the Number Game.

--- Guess_the_number_python.py
+++ Guess_the_number_python.py
@@ -1,26 +1,26 @@
 import random
 from time import sleep
 
-guess_limit = 4
+guess_limit = 4  # Set the guesses limit
 
 
 def game():
-    x = [random.randint(1, 10) for x in range(1, 40)]
+    x = [random.randint(1, 10) for x in range(1, 40)] # Generate intro random numbers
 
     for val in x:
         print(val, end=' ', flush=True)
         sleep(0.1)
-    secret_number = random.randint(1, 9)
+    secret_number = random.randint(1, 9) # Mystery Random Number
     guess_count = 1
-    while guess_count < guess_limit:
+    while guess_count < guess_limit:  # Ask for number till the guess counter is lower than guess limit
         try:
-            print(f'\nCount: {guess_count}')
+            print(f'\nCount: {guess_count}') 
             print('Guess the number')
             guess = int(input('>'))
-        except ValueError:
+        except ValueError:    # Pass if player enters no digit
             pass
         else:
-            guess_count += 1
+            guess_count += 1  #Counter Increment
             if guess == secret_number:
                 print('Great you won')
                 print(f' You guest the number in {guess_count} guesses')
