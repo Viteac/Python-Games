3c02f67075eb3e5bd527970ed7f686a5afadcf39
Vimuster <32931324+Viteac@users.noreply.github.com>
2021-05-29 23:18:24 +0100

    Guess The Number Python Game
    Added Intro Number Generator and Function asking user if Start New Game.

--- Guess_the_number_python.py
+++ Guess_the_number_python.py
@@ -8,17 +8,26 @@ guess_limit = 4  # Set the guesses limit
 
 
 def game():
+    
+    def ask():
+        while True:
+            again = input('Wanna play again Y/N?\n> ').lower()
+            if again in 'yn':
+                return again
+            else:
+                pass
+
     x = [random.randint(1, 10) for x in range(1, 40)] # Generate intro random numbers
 
     for val in x:
-        print(val, end=' ', flush=True)
+        print(val, end=' ', flush=True) # Flush intro x
         sleep(0.1)
     secret_number = random.randint(1, 11) # Mystery Random Number
     guess_count = 1
     while guess_count < guess_limit:  # Ask for number till the guess counter is lower than guess limit
         try:
             print(f'\nCount: {guess_count}') 
-            print('Guess the number between 1 - 10')
+            print('Guess the number 1-10')
             guess = int(input('>'))
         except ValueError:    # Pass if player enters no digit
             pass
@@ -27,13 +36,11 @@ def game():
             if guess == secret_number:
                 print('Great you won')
                 print(f' You guest the number in {guess_count} guesses')
-                ask = input('Want to play again? Y/N').upper()
-                if ask == 'Y':
+                if ask() == 'y':
                     return game()
     else:
         print(f'You had your limits mate, the secret number was {secret_number}')
-    ask = input('Want to play again? Y/N').upper()
-    if ask == 'Y':
+    if ask() == 'y':
         return game()
 
 
